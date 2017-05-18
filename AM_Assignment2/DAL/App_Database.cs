85ca3714b1c495923cbb923919cef6afbebcd0cd
shawee22 <shawee22@student.wintec.ac.nz>
2017-05-18 13:20:57 +1200

    Add create group functionality

--- AM_Assignment2/DAL/App_Database.cs
+++ AM_Assignment2/DAL/App_Database.cs
@@ -14,6 +14,8 @@ namespace AM_Assignment2.DAL
         public DbSet<Status> Status { get; set; }
         public DbSet<UserType> UserType { get; set; }
 
+        public DbSet<Group> Group { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
---
+++ AM_Assignment2/Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AM_Assignment2.Models
+{
+    //  Class definition for Group table
+    public class Group
+    {
+        public int GroupID { get; set; }
+        public string GroupName { get; set; }
+    }
+}
