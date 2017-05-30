b59f9cd89ef83b826deeb840635ecea4a3fce70e
shawee22 <shawee22@student.wintec.ac.nz>
2017-05-30 14:36:18 +1200

    Develop admin user startup script

--- AM_Assignment2/Startup.cs
+++ AM_Assignment2/Startup.cs
@@ -5,6 +5,7 @@ using AM_Assignment2.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using Microsoft.Owin.Security.Cookies;
+using System.Linq;
 
 [assembly: OwinStartupAttribute(typeof(AM_Assignment2.Startup))]
 namespace AM_Assignment2
@@ -55,6 +56,31 @@ namespace AM_Assignment2
                 {
                     var result = roleManager.Create(role);
                 }
+            }
+
+            // Get a list of existing application users and delete 
+            List<ApplicationUser> currentUsers = userManager.Users.ToList();
+            if (currentUsers != null)
+            {
+                foreach (ApplicationUser currentUser in currentUsers)
+                {
+                    userManager.Delete(currentUser);
+                }
+            }
+
+            // Create initial users
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser{ UserName="nicsco10@student.wintec.ac.nz", Email="nicsco10@student.wintec.ac.nz", Id="b2cfcc69-1959-4913-ae78-070d5e60af05", EmailConfirmed=true },
+                new ApplicationUser{ UserName="thowea10@student.wintec.ac.nz", Email="thowea10@student.wintec.ac.nz", Id="8b7efdb4-e0c8-40d0-ac17-4ab34c9da983", EmailConfirmed=true },
+                new ApplicationUser{ UserName="shawee22@student.wintec.ac.nz", Email="shawee22@student.wintec.ac.nz", Id="92f20888-702a-4b64-aad0-ffbd32462e2e", EmailConfirmed=true }
+            };
+
+            // Assign password and set all to admin roles
+            foreach (ApplicationUser user in users)
+            {
+                var result = userManager.Create(user, "password_1");
+                if (result.Succeeded) userManager.AddToRole(user.Id, "Administrator");
             }
         }
     }
