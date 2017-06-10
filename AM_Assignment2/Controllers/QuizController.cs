0d1abe5fcb36997b0df385097a1085107b5b6787
AlbertThePanda <tom.weake@hotmail.com>
2017-06-10 16:55:54 +1200

    Changes to add results page for the quiz

--- AM_Assignment2/Controllers/QuizController.cs
+++ AM_Assignment2/Controllers/QuizController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
-namespace AM_Assignment2.Controllers
+namespace AM_Assignment2
 {
     public class QuizController : Controller
     {
         // GET: Quiz
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Results(bool semantic, bool nonsemantic, string contactMethod, bool table, bool span, bool article)
+        {
+            ViewBag.ContactMethod = contactMethod;
+            var list = new List<String>();
+            var list2 = new List<String>();
+            if (semantic) list.Add("semantic - correct");
+            if (nonsemantic) list.Add("non-semantic - wrong");
+            if (table) list2.Add("table - wrong");
+            if (span) list2.Add("span - correct");
+            if (article) list2.Add("article - wrong");
+            ViewBag.InformMethods = list;
+            ViewBag.SomethingMethods = list2;
+            return View();
+        }
+
     }
 }
