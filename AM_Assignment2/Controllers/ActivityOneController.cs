9064648e023c06eefc5b6b8cdec11a4b18e0249f
AlbertThePanda <tom.weake@hotmail.com>
2017-06-10 17:18:45 +1200

    ActivityOne page added

---
+++ AM_Assignment2/Controllers/ActivityOneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AM_Assignment2.Controllers
+{
+    public class ActivityOneController : Controller
+    {
+        // GET: ActivityOne
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
