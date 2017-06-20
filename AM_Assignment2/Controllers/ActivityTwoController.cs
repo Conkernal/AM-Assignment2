a9bf25278eaadd94bb171777cf38ea270c7d4aad
AlbertThePanda <tom.weake@hotmail.com>
2017-06-21 11:42:23 +1200

    Added basic activitytwo controller and view

---
+++ AM_Assignment2/Controllers/ActivityTwoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AM_Assignment2.Controllers
+{
+    public class ActivityTwoController : Controller
+    {
+        // GET: ActivityTwo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
