b3d40d77433664e43f914541e7175472260fd24e
shawee22 <shawee22@student.wintec.ac.nz>
2017-06-23 14:41:44 +1200

    Removed unnecessary "usings"

--- AM_Assignment2/Models/Status.cs
+++ AM_Assignment2/Models/Status.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace AM_Assignment2.Models
+﻿namespace AM_Assignment2.Models
 {
     // Class definition of Status table
     public class Status
