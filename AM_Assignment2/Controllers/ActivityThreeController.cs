970460072ee139f30b9b5f44916a5910dbab5b06
AlbertThePanda <tom.weake@hotmail.com>
2017-06-24 10:50:13 +1200

    Added ActivityThree page

---
+++ AM_Assignment2/Controllers/ActivityThreeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AM_Assignment2.Controllers
+{
+    public class ActivityThreeController : Controller
+    {
+        // GET: ActivityThree
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
