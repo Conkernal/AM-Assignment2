da51469047b8f4e3ec10d362f3f308874dcb3894
AlbertThePanda <tom.weake@hotmail.com>
2017-06-28 11:46:08 +1200

    Added checkmark and x mark images for correct and incorrect results in the quiz.

--- AM_Assignment2/Controllers/QuizController.cs
+++ AM_Assignment2/Controllers/QuizController.cs
@@ -20,12 +20,68 @@ namespace AM_Assignment2
         [HttpPost]
         public ActionResult Results(string Answer1, string Answer2, string Answer3, string Answer4, string Answer5, string Answer6)
         {
+            
             ViewData["Answer1"] = Answer1;
+            if (Answer1 == "A")
+            {
+                ViewData["Answer1Result"] = "/Content/Images/checkmark.png";
+            }
+            else
+            {
+                ViewData["Answer1Result"] = "/Content/Images/x mark.png";
+
+            }
+
             ViewData["Answer2"] = Answer2;
+            if (Answer2 == "B")
+            {
+                ViewData["Answer2Result"] = "/Content/Images/checkmark.png";
+            }
+            else
+            {
+                ViewData["Answer2Result"] = "/Content/Images/x mark.png";
+
+            }
             ViewData["Answer3"] = Answer3;
+            if (Answer3 == "A")
+            {
+                ViewData["Answer3Result"] = "/Content/Images/checkmark.png";
+            }
+            else
+            {
+                ViewData["Answer3Result"] = "/Content/Images/x mark.png";
+
+            }
             ViewData["Answer4"] = Answer4;
+            if (Answer4 == "C")
+            {
+                ViewData["Answer4Result"] = "/Content/Images/checkmark.png";
+            }
+            else
+            {
+                ViewData["Answer4Result"] = "/Content/Images/x mark.png";
+
+            }
             ViewData["Answer5"] = Answer5;
+            if (Answer5 == "A")
+            {
+                ViewData["Answer5Result"] = "/Content/Images/checkmark.png";
+            }
+            else
+            {
+                ViewData["Answer5Result"] = "/Content/Images/x mark.png";
+
+            }
             ViewData["Answer6"] = Answer6;
+            if (Answer6 == "C")
+            {
+                ViewData["Answer6Result"] = "/Content/Images/checkmark.png";
+            }
+            else
+            {
+                ViewData["Answer6Result"] = "/Content/Images/x mark.png";
+
+            }
             return View();
         }
 
